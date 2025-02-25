efcef49d318ace04487a3000689bcd258dded3ac
VitorFanelli <vitorhugofinocencio@gmail.com>
2025-02-24 22:13:12 -0300

    Duas Instâncias

--- ClassesObjetos/src/App.java
+++ ClassesObjetos/src/App.java
@@ -1,10 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Caneta c1 = new Caneta();
 
+        Caneta c1 = new Caneta();
+        c1.modelo = "Bic";
         c1.cor = "Azul";
         c1.ponta = 0.5f;
-        c1.tampada = false;
+        c1.tampar();
         c1.status();
+        c1.rabiscar();
+
+
+        Caneta c2 = new Caneta();
+        c2.modelo = "Hollywod";
+        c2.cor = "Preta";
+        c2.ponta = 0.5f;
+        c2.destampar();
+        c2.status();
+        c2.rabiscar();
+
+
     }
 }
--- ClassesObjetos/src/Caneta.java
+++ ClassesObjetos/src/Caneta.java
@@ -6,20 +6,29 @@ public class Caneta {
     boolean tampada;
 
     void status(){
+        System.out.println("Modelo: " + this.modelo);
         System.out.println("Uma Caneta " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
         System.out.println("Esta tampada? " + this.tampada);
     }
 
-    void rabiscar(){
+    void rabiscar(){ 
+        if(this.tampada==true){
+            System.out.println("Erro!! Não posso rabiscar");
+        }else{
+            System.out.println("Rabiscar!!");
+        }
 
     }
 
     void tampar(){
-
+        this.tampada = true;
+        // this referencia ao Objeto
     }
 
     void destampar(){
-
+        this.tampada = false;
     }
 
 }
