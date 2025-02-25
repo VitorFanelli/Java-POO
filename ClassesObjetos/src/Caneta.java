a0fe33a21d8e5d31b132c02784445955920c8771
VitorFanelli <vitorhugofinocencio@gmail.com>
2025-02-24 21:24:40 -0300

    Class Caneta

---
+++ ClassesObjetos/src/Caneta.java
@@ -0,0 +1,25 @@
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+
+    void status(){
+        System.out.println("Uma Caneta " + this.cor);
+        System.out.println("Esta tampada? " + this.tampada);
+    }
+
+    void rabiscar(){
+
+    }
+
+    void tampar(){
+
+    }
+
+    void destampar(){
+
+    }
+
+}
